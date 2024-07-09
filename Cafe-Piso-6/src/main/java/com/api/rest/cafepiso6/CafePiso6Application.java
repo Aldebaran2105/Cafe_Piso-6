379aaa7b23596d443cc25b2151096ec6afb30c4f
_Aldebaran_ <santiago.cama@utec.edu.pe>
2024-07-09 14:10:57 -0500

    First Commit

---
+++ Cafe-Piso-6/src/main/java/com/api/rest/cafepiso6/CafePiso6Application.java
@@ -0,0 +1,13 @@
+package com.api.rest.cafepiso6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafePiso6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafePiso6Application.class, args);
+	}
+
+}
